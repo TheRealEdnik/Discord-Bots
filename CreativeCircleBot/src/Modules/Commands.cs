d74af1399fe9296eb04f54ae3f02ec2e1c18e9fa
TheRealEdnik <therealednik@gmail.com>
2021-05-15 20:58:25 +0200

    add Discord Bot

---
+++ CreativeCircleBot/src/Modules/Commands.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Modules
+{
+    [Name("Commands")]
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("say"), Alias("s")]
+        [Summary("Make the bot say something")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public Task Say([Remainder] string text)
+            => ReplyAsync(text);
+
+        [Command("invites"), Alias ("inv")]
+        public async Task Invites()
+        {
+            var invitesRole = Context.Guild.Roles.FirstOrDefault(x => x.Id == 839474198057320448);
+
+            if (!(Context.User as SocketGuildUser).Roles.Any(x => x.Id == 839474198057320448))
+            {
+                await Context.Channel.SendMessageAsync("You can't get anymore invites!");
+                return;
+            }
+
+            var inv1 = await ((Context.Guild as SocketGuild).GetChannel(839016058454278167) as ITextChannel).CreateInviteAsync(null, 1, false, true) as IInvite;
+            var inv2 = await ((Context.Guild as SocketGuild).GetChannel(839016058454278167) as ITextChannel).CreateInviteAsync(null, 1, false, true) as IInvite;
+
+            await (Context.User as SocketGuildUser).SendMessageAsync($"Here you have your invite links: \n\n{inv1.Url}\n{inv2.Url}\nMake sure to use them wisely!");
+
+            await (Context.User as SocketGuildUser).RemoveRoleAsync(invitesRole as IRole);
+        }
+    }
+}
---
+++ CreativeCircleBot/src/Services/CommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace Example
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+        private readonly IServiceProvider _provider;
+
+        // DiscordSocketClient, CommandService, IConfigurationRoot, and IServiceProvider are injected automatically from the IServiceProvider
+        public CommandHandler(
+            DiscordSocketClient discord,
+            CommandService commands,
+            IConfigurationRoot config,
+            IServiceProvider provider)
+        {
+            _discord = discord;
+            _commands = commands;
+            _config = config;
+            _provider = provider;
+
+            _discord.UserJoined += OnUserJoin;
+            _discord.MessageReceived += OnMessageReceivedAsync;
+        }
+
+        private async Task OnUserJoin(SocketGuildUser user)
+        {
+            var invitesRole = (_discord.GetGuild(839016057901023263) as SocketGuild).Roles.FirstOrDefault(x => x.Name.ToString() == "invites");
+            var userRole = (_discord.GetGuild(839016057901023263) as SocketGuild).Roles.FirstOrDefault(x => x.Name.ToString() == "User");
+
+            await user.AddRoleAsync(invitesRole);
+            await user.AddRoleAsync(userRole);
+        }
+
+        private async Task OnMessageReceivedAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;     // Ensure the message is from a user/bot
+            if (msg == null) return;
+            if (msg.Author.Id == _discord.CurrentUser.Id) return;     // Ignore self when checking commands
+            
+            var context = new SocketCommandContext(_discord, msg);     // Create the command context
+
+            int argPos = 0;     // Check if the message has a valid command prefix
+            if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _provider);     // Execute the command
+
+                if (!result.IsSuccess)     // If not successful, reply with the error.
+                    await context.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+    }
+}
